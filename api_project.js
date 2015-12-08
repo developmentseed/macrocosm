a0fca007c16c3631256bdd02b2ce05692c2620c8
Travis-CI <travis@somewhere.com>
2015-12-08 16:40:59 +0000

    CI deploy to gh-pages

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "macrocosm",
+  "version": "0.0.1",
+  "description": "The OSM API as a platform in NodeJS",
+  "title": "Macrocosm API documentation",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-12-08T16:40:59.069Z",
+    "url": "http://apidocjs.com",
+    "version": "0.12.3"
+  }
+});
