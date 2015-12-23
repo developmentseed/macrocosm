957bca1628b7975a3163226d2197c6852903468a
Seth Fitzsimmons <seth@mojodna.net>
2015-12-23 10:12:36 -0800

    PUT /api/0.6/changeset/create

--- routes/changeset-create.js
+++ routes/changeset-create.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var Boom = require('boom');
+var xml2js = require('xml2js');
+
 var knex = require('../connection.js');
 
 module.exports = {
@@ -13,19 +15,64 @@ module.exports = {
    * @apiVersion 0.1.0
    *
    * @apiParam {Number} uid User ID
-   * @apiParam {String} user User name 
+   * @apiParam {String} user User name
    *
    * @apiSuccess {Number} id Created changeset ID
    *
-   * @apiExample {curl} Example Usage: 
-   *    curl -X PUT --data "uid=1&user=openroads" http://localhost:4000/changeset/create 
+   * @apiExample {curl} Example Usage:
+   *    curl -X PUT --data "uid=1&user=openroads" http://localhost:4000/changeset/create
    *
    * @apiSuccessExample {json} Success-Response:
    *  {"id":"1194"}
    */
   method: 'PUT',
-  path: '/changeset/create',
+  path: '/api/0.6/changeset/create',
   handler: function changesetCreate(req, res) {
+    if (req.mime === "text/xml") {
+      return xml2js.parseString(req.payload, function(err, payload) {
+        if (err) {
+          return res(Boom.wrap(err));
+        }
+
+        var changeset = payload.osm.changeset[0];
+        var uid = 1; // this would be provided by OAuth if implemented
+        var now = new Date();
+
+        return knex('changesets')
+          .returning('id')
+          .insert({
+            user_id: uid,
+            created_at: now,
+            closed_at: now,
+            num_changes: 0
+          })
+          .then(function(ids) {
+            if (ids.length < 1) {
+              throw new Error('Could not add changeset to database.');
+            }
+
+            return ids[0];
+          }).then(function(id) {
+            var tags = changeset.tag.map(function(tag) {
+              return {
+                changeset_id: id,
+                k: tag.$.k,
+                v: tag.$.v
+              };
+            });
+
+            return knex('changeset_tags')
+              .insert(tags)
+              .then(function() {
+                return res(id).type('text/plain');
+              });
+          })
+          .catch(function(err) {
+            return res(Boom.wrap(err));
+          });
+      });
+    }
+
     var now = new Date();
     var uid = req.payload.uid;
     var userName = req.payload.user;
