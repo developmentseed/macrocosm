d3915c79b71b77561e0803e02dfb2a5e3c31bf79
Seth Fitzsimmons <seth@mojodna.net>
2015-12-31 16:15:15 -0800

    Introduce debug for scoped debug output

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
+
+var _debug = require('debug');
 var Hapi = require('hapi');
+var util = require('util');
 var xml2json = require('xml2json');
+
+var meta = require('./package.json');
+var debug = _debug(util.format('%s:http', meta.name));
 
 var server = new Hapi.Server({
   connections: {
@@ -16,6 +22,12 @@ var server = new Hapi.Server({
 
 server.connection({ port: process.env.PORT || 4000 });
 
+
+server.ext('onRequest', function(req, res) {
+  debug('href: %s', req.url.href);
+  return res.continue();
+});
+
 // Register routes
 server.register({
   register: require('hapi-router'),
@@ -28,6 +40,7 @@ server.register({
 
 server.ext('onPostAuth', function(req, res) {
   if (req.mime === 'text/xml' && Object.keys(req.payload || {}).length > 0) {
+    debug('payload: %s', req.payload);
     req.payload = xml2json.toJson(req.payload, {
       object: true
     });
